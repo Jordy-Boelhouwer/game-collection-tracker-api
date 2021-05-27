0072885a0e6df3da4f103e2565cba9fed7299d12
Jordy Boelhouwer <jordyboelhouwer@hotmail.com>
2021-05-27 15:39:43 +0200

    feat(genres): add genre module endpoints with static data

---
+++ src/genres/genres.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { GenresService } from './genres.service';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+
+@Controller('genres')
+export class GenresController {
+  constructor(private readonly genresService: GenresService) {}
+
+  @Get()
+  findAll() {
+    return this.genresService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.genresService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() genre: CreateGenreDto) {
+    return this.genresService.create(genre);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() genre: UpdateGenreDto) {
+    return this.genresService.update(Number(id), genre);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.genresService.remove(Number(id));
+  }
+}
